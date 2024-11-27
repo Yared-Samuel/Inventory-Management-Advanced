60a2856278fa2fc1e06174d3aae4268fcb4a1e4d
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2024-11-27 15:12:17 +0300

    navBar and menu

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,14 +11,18 @@ export default function DashboardLayout({
     return (
       <div className="h-screen flex">
         {/* Left */}
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-100">
-         <Link href={"/"} className="flex items-conter justify-center gap-2">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+         <Link href={"/"} className="flex items-center justify-center lg:justify-start gap-2">
           <Image src={"/logo.png"} alt="logo" width={32} height={32} />
-         </Link> 
          <span className="hidden lg:block">StoreName</span>
+         </Link> 
+         <Menu />
         </div>
         {/* Right */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200"></div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar />
+          {children}
+        </div>
       </div>
     );
   }
