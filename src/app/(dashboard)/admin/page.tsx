065801524f514aa9f8605cc9ed023c38378c8fd2
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2024-11-28 19:54:18 +0300

    chartZZ

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from '@/components/AttendanceChart'
 import CountChart from '@/components/CountChart'
+import FinanceChart from '@/components/FinanceChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -16,7 +17,7 @@ const AdminPage = () => {
           <UserCard type="staff" />
         </div>
         {/* MIDDLE CHARTS  */}
-        <div className='flex gap-4 flex-col lg:flex-row'></div>
+        <div className='flex gap-4 flex-col lg:flex-row'>
         {/* COUNT CHARTS  */}
         <div className='w-full lg:w-1/3 h-[450px]'>
           <CountChart />
@@ -24,6 +25,11 @@ const AdminPage = () => {
         {/* ATTENDANCE CHARTS  */}
         <div className='w-full lg:w-2/3 h-[450px]'>
           <AttendanceChart />
+        </div>
+        </div>
+        {/* BOTTOM CHART  */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
 
