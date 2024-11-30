99ad3a8d04d4ffbfcc7ce89a09496e0e33b21869
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2024-11-30 13:17:25 +0300

    overall pages

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,8 +1,24 @@
+import Announcements from '@/components/Announcements'
+import BigCalendar from '@/components/BigClander'
+import EventCalendar from '@/components/EventCalendar'
 import React from 'react'
 
 const StudentPage = () => {
   return (
-    <div>StudentPage</div>
+    <div className='p-4 flex gap-4 flex-col xl:flex-row'>
+      {/* LEFT  */}
+      <div className="w-full xl:w-2/3">
+      <div className="h-full bg-wite p-4 rouded-md">
+        <h1 className='text-xl font-semibold'>Schedul (4A)</h1>
+        <BigCalendar />
+      </div>
+      </div>
+      {/* RIGHT  */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+      </div>
   )
 }
 
